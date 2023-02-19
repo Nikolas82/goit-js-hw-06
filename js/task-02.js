41c147026ebadfc300bbc8f4b3cde23af04c5585
Nikolas82 <nikobogdan34@gmail.com>
2023-02-19 09:51:11 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const arrayIngred = document.querySelector("ul");
+
+const listEl = ingredients.map(ingredient =>{
+
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredient;
+
+  return itemEl;
+}
+
+)
+
+arrayIngred.append(...listEl);
+
+

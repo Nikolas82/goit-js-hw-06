9aff46f9f85712cac08250858d47950429cdd02b
Nikolas82 <nikobogdan34@gmail.com>
2023-02-19 19:32:07 +0200

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,19 @@
+const value = document.querySelector("#value");
+const decrementBtn = document.querySelector('[data-action = "decrement"]');
+const incrementBtn = document.querySelector('[data-action = "increment"]');
+
+let counterValue = 0;
+
+const decrementCounter = () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+};
+
+const incrementCounter = () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+};
+
+decrementBtn.addEventListener("click", decrementCounter);
+
+incrementBtn.addEventListener("click", incrementCounter);

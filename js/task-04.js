ef0b66d512002d66e2f26a209cdd85ef89be94da
Nikolas82 <nikobogdan34@gmail.com>
2023-02-20 17:17:05 +0200

    task 5

--- js/task-04.js
+++ js/task-04.js
@@ -4,6 +4,8 @@ const incrementBtn = document.querySelector('[data-action = "increment"]');
 
 let counterValue = 0;
 
+console.log(decrementBtn)
+
 const decrementCounter = () => {
   counterValue -= 1;
   value.textContent = counterValue;

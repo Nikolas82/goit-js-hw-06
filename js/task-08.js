6c3d45b83fd269fa825f8818292e64a218aec19a
Nikolas82 <nikobogdan34@gmail.com>
2023-02-21 18:10:07 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+
+  if (email === "" || password === "") {
+    alert("Всі поля повинні бути заповнені.");
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  form.reset();
+}

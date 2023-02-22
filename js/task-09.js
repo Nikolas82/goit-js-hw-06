e8266b566f07f3ac318bd9d02c7a6fee9693a231
Nikolas82 <nikobogdan34@gmail.com>
2023-02-22 22:41:48 +0200

    task 9 refactoring

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
 const bodyEl = document.querySelector("body");
 const colorBtn = document.querySelector(".change-color");
 const span = document.querySelector(".color");
 
 colorBtn.addEventListener("click", () => {
-  bodyEl.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
+  const color = getRandomHexColor();
+  bodyEl.style.backgroundColor = color;
+  span.textContent = color;
 });
